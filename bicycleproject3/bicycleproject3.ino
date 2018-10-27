ce375487c1b86ffd8d7698cb2af240dee25965f0
Brian <brianjbryson@gmail.com>
2018-10-27 08:30:57 -0700

    serial print works

--- bicycleproject3/bicycleproject3.ino
+++ bicycleproject3/bicycleproject3.ino
@@ -6,10 +6,14 @@
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-
+	Serial.begin(9600);
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
-  
+	Serial.println("hello world");
+	
+	int timenow = millis();
+	Serial.println(timenow);
+	delay(100);
 }
